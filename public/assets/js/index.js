7fadc82de4c6fd5204e9fbc26a4a72307fc73b6c
mkdkck <colin173@gmail.com>
2023-11-27 00:38:14 +1030

    created index.js& notes.js routes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -182,7 +182,7 @@ const renderNoteList = async (notes) => {
 };
 
 // Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const getAndRenderNotes = () => getNotes(data).then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
