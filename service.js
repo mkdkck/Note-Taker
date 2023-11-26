5e4e8c924ca922f2b9d7baf3cbce24ffc887be2e
mkdkck <colin173@gmail.com>
2023-11-27 01:00:38 +1030

    Revert "created index.js& notes.js routes"
    This reverts commit 7fadc82de4c6fd5204e9fbc26a4a72307fc73b6c.

--- service.js
+++ service.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT ||3001; 
+const port = 3001; 
 const fs = require('fs');
-const api = require('./routes/index.js')
 
 
 const app = express();
@@ -11,13 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-app.use('/api', api);
 
 
 
-app.get ('/', (req,res)=> {
-  res.sendFile(path.join(__dirname,'/public/index.html'))
-})
+
+
 
 
 
