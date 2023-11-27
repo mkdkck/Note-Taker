ac4e1ebad6b65c3dd581789fcad20b38666a81ee
mkdkck <colin173@gmail.com>
2023-11-27 15:43:00 +1030

    fix small bug

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.get ('/', (req,res)=> {
   res.sendFile(path.join(__dirname,'/public/index.html'))
 })
 
-app.get ('/note', (req,res)=> {
-  res.sendFile(path.join(__dirname,'/public/note.html'))
+app.get ('/notes', (req,res)=> {
+  res.sendFile(path.join(__dirname,'/public/notes.html'))
 })
 
 app.get ('*', (req,res)=> {
